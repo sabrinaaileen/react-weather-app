5591c86af179ff99396a5ff63293695e5214bb1f
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2023-05-28 22:40:11 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ export default function App() {
       <header className="App-header">
         <h1>React Weather App</h1>
       </header>
+      <footer>
+        <p>
+          {" "}
+          <a
+            href="https://github.com/sabrinaaileen/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source Code
+          </a>{" "}
+          from Sabrina Aileen Hodapp
+        </p>
+      </footer>
     </div>
   );
 }
